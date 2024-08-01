5967d442a026f1bda3b6c053dc0b440120d20ae5
Luke_YS <yjsmk0902@gmail.com>
2024-08-01 13:18:36 +0900

    [Week2][JavaToKotlin] 39강 Querydsl 사용하기 - 두번째 방법

--- 양승민/library-app/src/main/kotlin/com/group/libraryapp/domain/book/BookRepository.kt
+++ 양승민/library-app/src/main/kotlin/com/group/libraryapp/domain/book/BookRepository.kt
@@ -5,9 +5,4 @@ import org.springframework.data.jpa.repository.JpaRepository
 interface BookRepository : JpaRepository<Book, Long> {
 
   fun findByName(bookName: String): Book?
-
-  @Query("SELECT NEW com.group.libraryapp.dto.response.BookStatResponse(b.type, COUNT(b.id)) " +
-          "FROM Book b GROUP BY b.type")
-  fun getStats(): List<BookStatResponse>
-
 }
