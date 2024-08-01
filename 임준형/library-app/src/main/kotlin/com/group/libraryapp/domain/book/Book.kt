d83c0186356af59f66430d1aba83f237d795e3fb
toychip <dlawnsgud427@naver.com>
2024-08-01 18:39:57 +0900

    [Week2][JavaToKotlin] 21강 책의 분야 추가하기

--- 임준형/library-app/src/main/kotlin/com/group/libraryapp/domain/book/Book.kt
+++ 임준형/library-app/src/main/kotlin/com/group/libraryapp/domain/book/Book.kt
@@ -1,6 +1,5 @@
 package com.group.libraryapp.domain.book
 
-import java.lang.IllegalArgumentException
 import javax.persistence.Entity
 import javax.persistence.GeneratedValue
 import javax.persistence.GenerationType
@@ -9,6 +8,7 @@ import javax.persistence.Id
 @Entity
 class Book(
     val name: String,
+    val type: String,
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long? = null,
@@ -17,6 +17,16 @@ class Book(
     init {
         if (name.isBlank()) {
             throw IllegalArgumentException("이름은 비어 있을 수 없습니다.")
+        }
+    }
+
+    companion object{
+        fun fixture(
+            name: String = "책 이름",
+            type: String = "COMPUTER",
+            id: Long? = null
+        ): Book {
+            return Book(name, type, id)
         }
     }
 }
