1d8b285165bab12cf5cdf32299fed48fb19d16e1
Luke_YS <yjsmk0902@gmail.com>
2024-08-01 13:11:04 +0900

    [Week2][JavaToKotlin] 35강 애플리케이션 대신 DB로 기능 구현하기

--- 양승민/library-app/src/main/kotlin/com/group/libraryapp/domain/book/BookRepository.kt
+++ 양승민/library-app/src/main/kotlin/com/group/libraryapp/domain/book/BookRepository.kt
@@ -6,4 +6,8 @@ interface BookRepository : JpaRepository<Book, Long> {
 
   fun findByName(bookName: String): Book?
 
+  @Query("SELECT NEW com.group.libraryapp.dto.response.BookStatResponse(b.type, COUNT(b.id)) " +
+          "FROM Book b GROUP BY b.type")
+  fun getStats(): List<BookStatResponse>
+
 }
