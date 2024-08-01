4564eb9f93e97efec874fe7859a084aec40ca58f
Luke_YS <yjsmk0902@gmail.com>
2024-08-01 13:13:32 +0900

    [Week2][JavaToKotlin] 38강 Querydsl 사용하기 - 첫번째 방법

--- 양승민/library-app/src/main/kotlin/com/group/libraryapp/domain/user/UserRepository.kt
+++ 양승민/library-app/src/main/kotlin/com/group/libraryapp/domain/user/UserRepository.kt
@@ -5,7 +5,4 @@ import org.springframework.data.jpa.repository.JpaRepository
 interface UserRepository : JpaRepository<User, Long>, UserRepositoryCustom {
 
   fun findByName(name: String): User?
-
-  @Query("SELECT DISTINCT u FROM User u LEFT JOIN FETCH u.userLoanHistories")
-  fun findAllWithHistories(): List<User>
 }
